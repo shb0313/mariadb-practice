aadf631dda75cd8f3521e4579bc4da8f59daef0e
shb7815 <shb7815@gmail.com>
2023-01-11 16:08:25 +0900

    JDBC #1

--- jdbc-practice/src/main/java/test/ConnectionTest.java
+++ jdbc-practice/src/main/java/test/ConnectionTest.java
@@ -5,10 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionTest {
-	
-	
-	
-	
 	public static void main(String[] args) {
 		Connection conn = null;
 		
@@ -17,24 +13,22 @@ public class ConnectionTest {
 			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url = "jdbc:mariadb://192.168.10.115:3307/webdb?charset-utf8";
+			String url = "jdbc:mariadb://192.168.10.125:3307/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			System.out.println("연결 성공");
-			
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패: " + e);
+			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
-			System.out.println("error: " + e);
+			System.out.println("error:" + e);
 		} finally {
 			try {
 				if(conn != null) {
-					conn.close();					
+					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
 }
