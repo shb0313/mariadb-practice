ffb1f24cf6212dd545aa07ca1fe1a7dea6ade3eb
shb7815 <shb7815@gmail.com>
2023-01-11 14:53:03 +0900

    jdbc

---
+++ jdbc-practice/src/main/java/test/DeptVo.java
@@ -0,0 +1,29 @@
+package test;
+
+public class DeptVo {
+	private long no;
+	private String name;
+	
+	public long getNo() {
+		return no;
+	}
+	public void setNo(long no) {
+		this.no = no;
+	}
+	@Override
+	public String toString() {
+		return "DeptVo [no=" + no + ", name=" + name + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+}
